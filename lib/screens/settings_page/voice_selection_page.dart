09efd3d116ec5ddcddd2828be9ad4d07defaf534
ExitTrance <dearz@hotmail.de>
2020-06-28 18:59:42 +0100

    Completed Voice Selection.

--- lib/screens/settings_page/voice_selection_page.dart
+++ lib/screens/settings_page/voice_selection_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hack2020/constants.dart';
+import 'package:global_configuration/global_configuration.dart';
 
 class VoiceSelectionPage extends StatelessWidget {
   @override
@@ -34,11 +35,81 @@ class VoiceSelector extends StatefulWidget {
 //TODO: Implement Initial State of what's in the settings.json
 
 class _VoiceSelectorState extends State<VoiceSelector> {
-  String selected;
+  String selectedVoice;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedVoice = GlobalConfiguration().getString("voice");
+  }
+
+  void updateVoice(String voice) {
+    voice != GlobalConfiguration().getString("voice")
+        ? setState(() {
+            selectedVoice = voice;
+            GlobalConfiguration().updateValue("voice", voice);
+            print(selectedVoice);
+          })
+        : {};
+  }
+
+//GlobalConfiguration().updateValue("voice", "Mizuki");
   @override
   Widget build(BuildContext context) {
     return Column(
-      
+      children: <Widget>[
+        ListOptions(
+          title: "Justin [English]",
+          selectedVoice: selectedVoice,
+          updateVoice: updateVoice,
+        ),
+        ListOptions(
+          title: "Maja [Polish]",
+          selectedVoice: selectedVoice,
+          updateVoice: updateVoice,
+        ),
+        ListOptions(
+          title: "Enrique [Spanish]",
+          selectedVoice: selectedVoice,
+          updateVoice: updateVoice,
+        ),
+        ListOptions(
+          title: "Marlene [German]",
+          selectedVoice: selectedVoice,
+          updateVoice: updateVoice,
+        ),
+        ListOptions(
+          title: "Mizuki [Japanese]",
+          selectedVoice: selectedVoice,
+          updateVoice: updateVoice,
+        )
+      ],
+    );
+  }
+}
+
+class ListOptions extends StatelessWidget {
+  ListOptions(
+      {@required this.title,
+      @required this.selectedVoice,
+      @required this.updateVoice});
+  final String title;
+  final String selectedVoice;
+  final Function updateVoice;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(color: kPrimaryWhite),
+      ),
+      leading: Radio(
+        value: title.split(new RegExp(r" "))[0],
+        groupValue: selectedVoice,
+        onChanged: updateVoice,
+      ),
+      onTap: () => updateVoice(title.split(new RegExp(r" "))[0]),
     );
   }
 }
